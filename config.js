252b169b97cc6118061e83fb8bac29960f363802
lastsymphony <78311812+lastsymphony@users.noreply.github.com>
2024-12-06 19:32:52 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,126 @@
+// controls //
+global.owner = ['6289670394574']
+global.ownername = "katheryne"
+global.connect = true // ubah ke false klo mau connect lewat qrcode 
+global.welcome = true
+global.antilink = false
+
+// hosting //
+      // panel //
+global.domain = "" // jan pke / di akhiran domain
+global.apikey = "" 
+global.capikey = ""
+global.eggsnya = '15' 
+global.location3 = '1'
+
+// api digital ocean
+global.token_do = "-"
+
+ // panel server 2 //
+global.domain2 = ""
+global.apikey2 = ""
+global.capikey2 = ""
+global.egg2 = '15' 
+global.loc2 = '1' 
+
+// payment gateway //
+global.merchantIdOrderKuota = "OK2060177"
+global.apiOrderKuota = "430694417318329852060177OKCT827C2279DDFCD8D6DD9E2D6C1BDC7D44"
+global.qrisOrderKuota = "00020101021126670016COM.NOBUBANK.WWW01189360050300000879140214009121195553790303UMI51440014ID.CO.QRIS.WWW0215ID20243485064430303UMI5204541153033605802ID5923ANGGARA STORE OK20601776013JAKARTA BARAT61051111062070703A01630444F6"
+
+ // subdomain //
+global.subdomain = {
+"reseller-panel.me": {
+"zone": "b1efab0f1d81a0137eb5d62e00d6e3f8",
+"apitoken": "W3b66ootKk1pERNSB6DbyW383OXRCoakw3PQExyS"
+},
+"market-panel.site": {
+"zone": "d06bf5450ae51612a400bab1c4450283",
+"apitoken": "kmb6AkpJ6XvHMzw2m0KbYKZOycIURNYMPA7Wm0BE",
+},
+"admin-panel.tech": {
+"zone": "305d4757160a88486d3780785c7c9887",
+"apitoken": "UH-aDtxu5Mm9oU8khMd5ZVg22f7nhnSkCORBn8zP",
+},
+"kang-panel.tech": {
+"zone": "28baf36eb9ced4be271bdb6ea3320f41",
+"apitoken": "MBiKuyoS0e0zJc8LKUjSXyLmgAf1IV9u86FJmVny",
+},
+"web-cabul.me": {
+"zone": "4b24636040f0f2d799a8a1aaea568e57",
+"apitoken": "OvRX-DghNjcPF9rlKElGCLIsPvv3Nd3khuDAUZ3q",
+},
+
+"king-hosting.live": {
+"zone": "2e6eb183148e0ef9add390af271a8bb2",
+"apitoken": "kcnnE1sESybx-P_nLkkiKtfZFqGhRmwFg9wL0cf6",
+},
+"tokopanel.software": {
+"zone": "cc9638d4c289130ba070484625e6aefa",
+"apitoken": "bcAP5z3gHvmQTlQ59qNB9BWmf0JiGt0C99FU6SXs",
+},
+"pannel-private.me": {
+"zone": "71ad41d3c25085bd7b8a1135632c7c63",
+"apitoken": "HfGKPUu1KrOVc3q51nkoJYZFQvIpA-AFOP-t6SNZ",
+},
+"hitam.systems": {
+"zone": "cfaba3ef0065acc82facc17f1b79d50b",
+"apitoken": "4o3tZAc5jOzA00joKpLYhG_616qCJxNLGlhawMEY",
+},
+"panel-run-bot.engineer": {
+"zone": "678298eaac2e2a0dea25f693310ec6e0",
+"apitoken": "8UmNiZZqEIAqWbM7XlcZofyvhc70NMs2_gXmmkug",
+},
+"uchiha.tech": {
+"zone": "1b09e81ec29d760ff33e476a084de6ed",
+"apitoken": "nzLwv-2uzMt3Ihsd5MVV2aLJ9EoovxrVK7Y4b2To",
+},
+"buyer-vps.site": {
+"zone": "354e1c784bed5a8d93f458ec1ff86f35",
+"apitoken": "rzOLIO1c-Me1X0owStLFUZ0ggfs_cxHDab2A7krF",
+},
+"arzanoffc.online": {
+"zone": "43ca801f5c6a73a77da5f0243f4f8c1d",
+"apitoken": "RZQft_gJGOJDskx6L6SxREJCvOKw0hclLjHUCNLS",
+},
+"hexaneuro.biz.id": {
+"zone": "16273869fda58585fc435e4582a5ff56",
+"apitoken": "y4wV0um-bvrVTvFVPrNrGEi590bPowvosRyNiGMn"
+ },   
+"mypanel.tech": {
+"zone": "9c5460deca7c84273b46f2f047783636",
+"apitoken": "0IBfmlUlCkYqoFPWYaqCmqI3_vWxkrFNbxrQjzX2"
+}, 
+"kedai-panel.me": {
+"zone": "3ab91ea96368234719c9de7d260742e2",
+"apitoken": "O22A19W7trZZ52bAXtF77aUVdGDdAk8PjQqwM7qv"
+}, 
+"control-pannel.site": {
+"zone": "20c517d7dcc25d9cf516f5c4cf2d21ba",
+"apitoken": "mAA84Qs4nJvLTaup0HTGrpVbgaSk-4vfUrcdHgCH"
+}, 
+"mypanelstore.xyz": {
+"zone": "72f7362d7686d9158a8e389851fc379c", 
+"apitoken": "V2y-I2o1s5E1tdCPpUSnT4FokV6WuxEUC2FqqVIX"
+}
+}
+
+// pays //
+global.dana = ""
+global.gopay = ""
+global.ovo = ""
+global.qris = ""
+
+// misc //
+global.versionofscript = "1.6" 
+global.url = "https://telegra.ph/file/101ddaef01c2557d9cbe8.jpg" // buat banner
+global.urlbanner = "https://telegra.ph/file/101ddaef01c2557d9cbe8.jpg" // buat banner 2
+global.url2 = "https://t.me/akangcupen" // isi url bebas buat reply
+global.packname = "Last " // sticker
+global.author = "katheryne " // sticker
+global.group = "" // isi group bebas lu
+global.idCH = "120363326439706177@newsletter"
+global.xchannel = {
+	jid: '120363326439706177@newsletter'
+	}
+	
